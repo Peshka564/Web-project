e5c51b67b862595b13f15a98211f430e6377935b
lastvoidtemplar <deyandelchev03@abv.bg>
2026-01-28 10:58:27 +0200

    simple fixes 4

--- src/config.php
+++ src/config.php
@@ -4,5 +4,5 @@ require_once __DIR__ . "/autoload.php";
 use Configuration\ConfigurationService;
 
 ConfigurationService::load(
-    __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "app.json"
+    __DIR__ . "/../config/app.json"
 );
